ffa12fc199079e982f8a2c4d7c404b6c7f95d7dc
Thanis <77865201+thanis04@users.noreply.github.com>
2024-08-03 14:55:30 +0000

    javascript private

--- start/01_05_private_object.js
+++ start/01_05_private_object.js
@@ -1,1 +1,12 @@
 // Write your code here
+const username = Symbol("username");
+const password = Symbol("password");
+
+const user = {
+  [username]: "Thanis",
+  [password]: "12345",
+  age: 24
+};
+
+console.log(user.username);
+console.log(user.password);
