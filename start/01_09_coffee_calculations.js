8a2154750a27019094859bfc04fafec872db0a5e
Thanis <77865201+thanis04@users.noreply.github.com>
2024-08-03 16:15:51 +0000

    reduce function

--- start/01_09_coffee_calculations.js
+++ start/01_09_coffee_calculations.js
@@ -1,1 +1,9 @@
 // Write your code here
+function coffeeDate(coffeeArr) {
+  let coffeeSum = coffeeArr.reduce(
+    (coffeeTotal, coffeeNum) => (coffeeTotal += coffeeNum)
+  );
+  return `The total bill is $${coffeeSum * 1.25}`;
+}
+
+console.log(coffeeDate([2, 3, 6, 9]));
