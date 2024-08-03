8d9081a4fa0381a6a3e1abd971fa074aab80df15
Thanis <77865201+thanis04@users.noreply.github.com>
2024-08-03 15:48:36 +0000

    array set

--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -1,1 +1,17 @@
 // Write your code here
+function getStudents(classRoom) {
+  let {hasTeachingAssistant, classList} = classRoom;
+  let teacher, teachingAssistant, students;
+
+  if (hasTeachingAssistant) {
+    [teacher, teachingAssistant, ...students] = classList;
+  } else {
+    [teacher, ...students] = classList;
+  }
+  return students;
+}
+
+console.log(getStudents({
+  hasTeachingAssistant: false,
+  classList: ["Rshida", "Jhon", "Roman", "Iqbal"]
+}));
