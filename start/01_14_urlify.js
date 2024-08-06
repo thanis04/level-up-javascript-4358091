12248fa6f5f8bd08c631789050cc14f2ff40de85
Thanis <77865201+thanis04@users.noreply.github.com>
2024-08-06 05:29:48 +0000

    punctuation remove

--- start/01_14_urlify.js
+++ start/01_14_urlify.js
@@ -1,1 +1,9 @@
 // Write your code here
+function urlify(blogTitle) {
+  const punctuation = /[.,/#!@%^&*()+=-_{'"}]/g;
+
+  const blogTitleWithoutPunctuation = blogTitle.replace(punctuation, "");
+  return blogTitleWithoutPunctuation.toLowerCase().trim().replaceAll(" ", "-");
+}
+
+console.log(urlify("I have to colombo!"));
