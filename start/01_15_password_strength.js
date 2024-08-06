6a7428ba66be085777ee535309b6db5570b02685
Thanis <77865201+thanis04@users.noreply.github.com>
2024-08-06 05:49:33 +0000

    valid password check

--- start/01_15_password_strength.js
+++ start/01_15_password_strength.js
@@ -1,1 +1,18 @@
 // Write your code here
+function checkPassword(password) {
+  let oneLowerCaseLetter = /(?=.+[a-z])/;
+  let oneUpperCaseLetter = /(?=.+[A-Z])/;
+  let oneDigit = /(?=.+[0-9])/;
+  let oneSpecialCharactor = /(?=.+[!@#$%^&*])/;
+  let minimumEightCharactor = /(?=.{8,})/;
+  let isValid = 
+    oneLowerCaseLetter.test(password) &&
+    oneUpperCaseLetter.test(password) &&
+    oneDigit.test(password) &&
+    oneSpecialCharactor.test(password) &&
+    minimumEightCharactor.test(password);
+
+  return isValid ? "Your password is valid" : "Your password is invalid";
+}
+
+console.log(checkPassword("asf324JK34^&"));
